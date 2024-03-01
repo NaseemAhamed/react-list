251fb4d37de04580b90b2f817b4d68ef14489f64
Naseem Ahamed Shahjahan <naseemahamed.shahj@stepstone.com>
2024-03-01 12:19:39 +0000

    styling

--- src/ItemForm.jsx
+++ src/ItemForm.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-export default function ItemForm({addToDoHandler}) {
+export default function ItemForm({ addToDoHandler }) {
     const [newItem, setNewItem] = useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
 
-        addToDoHandler(newItem);
+
         //Commenting this out because todo has to live in App.jsx because it is used for viewing there.
 
         // setTodos((currentTodos) => {
@@ -20,17 +20,19 @@ export default function ItemForm({addToDoHandler}) {
 
         //   ]
         // })
-
-        setNewItem("")
+        if (newItem.trim() != "") {
+            addToDoHandler(newItem);
+            setNewItem("")
+        }
     }
     return <form onSubmit={handleSubmit} className="new-item-form mb-4">
         <div className="form-row mb-2">
-            <label htmlFor="item"  className="text-white mr-2">Item to add</label>
+            <label htmlFor="item" className="text-white mr-2">Item to add</label>
             <input
                 value={newItem}
                 onChange={e => setNewItem(e.target.value)}
                 type="text" id="item" />
         </div>
-        <button  className="bg-blue-500 text-white px-2 py-1 rounded">Add</button>
+        <button className="bg-blue-500 text-white px-2 py-1 rounded">Add</button>
     </form>
 }
